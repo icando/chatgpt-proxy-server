94a6a9d7b726f724d12336b78e6b7b5fb4c1a597
wuzhangneng <wuzhangneng@youzan.com>
2023-03-12 21:18:44 +0800

    添加调用样例

--- client/main.go
+++ client/main.go
@@ -14,7 +14,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req := &api.ChatCompletionRequest{Model: openai.GPT3Dot5Turbo,
+	req := &api.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
 		Messages: []*api.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleUser,
